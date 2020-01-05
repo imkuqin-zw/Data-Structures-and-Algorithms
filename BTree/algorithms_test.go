4e038aca5b66380427ecc2c210457395209b2a77
imkuqin-zw <1141137429@qq.com>
2020-01-05 20:37:24 +0800

    btree

---
+++ BTree/algorithms_test.go
@@ -0,0 +1,77 @@
+package BTree
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestFindMaxSubTree(t *testing.T) {
+	type args struct {
+		root *BNode
+	}
+	type testParams struct {
+		name  string
+		arg   args
+		want  int
+		want1 *BNode
+	}
+	type createBTree func([]interface{}, int, int, int, **BNode) *BNode
+	var create createBTree
+	create = func(data []interface{}, start, end int, wantIndex int, want1 **BNode) *BNode {
+		var root *BNode
+		if start <= end {
+			mid := (start + end + 1) / 2
+			root = &BNode{
+				Data:   data[mid],
+				LChild: create(data, start, mid-1, wantIndex, want1),
+				RChild: create(data, mid+1, end, wantIndex, want1),
+			}
+			if mid == wantIndex {
+				*want1 = root
+			}
+		}
+		return root
+	}
+	fn := func(vals []interface{}, want int, wantIndex int) testParams {
+		var params testParams
+		params.arg = args{create(vals, 0, len(vals)-1, wantIndex, &params.want1)}
+		params.want = want
+		return params
+	}
+	var tests = []testParams{
+		fn([]interface{}{-1, 3, 6, -7}, 2, 1),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := FindMaxSubTree(tt.arg.root)
+			if got != tt.want {
+				t.Errorf("FindMaxSubTree() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("FindMaxSubTree() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestBTreeToDLink(t *testing.T) {
+	b := (*BNode)(CreateBTree([]interface{}{1, 2, 3, 4, 5, 6, 7}, 0, 6))
+	dl := BTreeToDLink(b)
+	cur := dl
+	var last = cur
+	for cur != nil {
+		fmt.Print(cur.Data, " ")
+		if cur.RChild != nil {
+			last = cur.RChild
+		}
+		cur = cur.RChild
+	}
+	fmt.Println()
+	for last != nil {
+		fmt.Print(last.Data, " ")
+		last = last.LChild
+	}
+	fmt.Println()
+}
---
+++ BTree/btree_test.go
@@ -0,0 +1,118 @@
+package BTree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBNode_LevelTraversal(t *testing.T) {
+	type fields struct {
+		node *BNode
+	}
+	fn := func(data []interface{}) fields {
+		return fields{(*BNode)(CreateBTree(data, 0, len(data)-1))}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []interface{}
+	}{
+		{"", fn([]interface{}{}), []interface{}{}},
+		{"", fn([]interface{}{1}), []interface{}{1}},
+		{"", fn([]interface{}{1, 2}), []interface{}{2, 1}},
+		{"", fn([]interface{}{1, 2, 3}), []interface{}{2, 1, 3}},
+		{"", fn([]interface{}{1, 2, 3, 4, 5, 6, 7}), []interface{}{4, 2, 6, 1, 3, 5, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := tt.fields.node
+			if got := b.LevelTraversal(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LevelTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBNode_PreOrderTraversal(t *testing.T) {
+	type fields struct {
+		node *BNode
+	}
+	fn := func(data []interface{}) fields {
+		return fields{(*BNode)(CreateBTree(data, 0, len(data)-1))}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []interface{}
+	}{
+		{"", fn([]interface{}{}), []interface{}{}},
+		{"", fn([]interface{}{1}), []interface{}{1}},
+		{"", fn([]interface{}{1, 2}), []interface{}{2, 1}},
+		{"", fn([]interface{}{1, 2, 3}), []interface{}{2, 1, 3}},
+		{"", fn([]interface{}{1, 2, 3, 4, 5, 6, 7}), []interface{}{4, 2, 1, 3, 6, 5, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := tt.fields.node
+			if got := b.PreOrderTraversal(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LevelTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBNode_InOrderTraversal(t *testing.T) {
+	type fields struct {
+		node *BNode
+	}
+	fn := func(data []interface{}) fields {
+		return fields{(*BNode)(CreateBTree(data, 0, len(data)-1))}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []interface{}
+	}{
+		{"", fn([]interface{}{}), []interface{}{}},
+		{"", fn([]interface{}{1}), []interface{}{1}},
+		{"", fn([]interface{}{1, 2}), []interface{}{1, 2}},
+		{"", fn([]interface{}{1, 2, 3}), []interface{}{1, 2, 3}},
+		{"", fn([]interface{}{1, 2, 3, 4, 5, 6, 7}), []interface{}{1, 2, 3, 4, 5, 6, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := tt.fields.node
+			if got := b.InOrderTraversal(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LevelTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBNode_PostOrderTraversal(t *testing.T) {
+	type fields struct {
+		node *BNode
+	}
+	fn := func(data []interface{}) fields {
+		return fields{(*BNode)(CreateBTree(data, 0, len(data)-1))}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []interface{}
+	}{
+		{"", fn([]interface{}{}), []interface{}{}},
+		{"", fn([]interface{}{1}), []interface{}{1}},
+		{"", fn([]interface{}{1, 2}), []interface{}{1, 2}},
+		{"", fn([]interface{}{1, 2, 3}), []interface{}{1, 3, 2}},
+		{"", fn([]interface{}{1, 2, 3, 4, 5, 6, 7}), []interface{}{1, 3, 2, 5, 7, 6, 4}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := tt.fields.node
+			if got := b.PostOrderTraversal(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LevelTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
