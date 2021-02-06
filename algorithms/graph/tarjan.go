15d7eaa678bf53e192f665ef67b11d63331f6d03
zhangwei <1141137429@qq.com>
2021-02-06 11:14:23 +0800

    tarjan

--- algorithms/graph/tarjan.go
+++ algorithms/graph/tarjan.go
@@ -2,7 +2,7 @@ package graph
 
 import "github.com/imkuqin-zw/Data-Structures-and-Algorithms/data_structure/graph"
 
-func Tarjan(g *graph.CFSGraph, p, f int, num *int, dfn, low *[]int) []*graph.CFSEdge {
+func UTarjan(g *graph.CFSGraph, p, f int, num *int, dfn, low *[]int) []*graph.CFSEdge {
 	result := make([]*graph.CFSEdge, 0)
 	*num++
 	(*dfn)[p], (*low)[p] = *num, *num
@@ -11,12 +11,41 @@ func Tarjan(g *graph.CFSGraph, p, f int, num *int, dfn, low *[]int) []*graph.CFS
 			continue
 		}
 		if (*dfn)[g.Edge[i].To] == 0 {
-			result = append(result, Tarjan(g, g.Edge[i].To, p, num, dfn, low)...)
+			result = append(result, UTarjan(g, g.Edge[i].To, p, num, dfn, low)...)
 			if (*low)[p] > (*low)[g.Edge[i].To] {
 				(*low)[p] = (*low)[g.Edge[i].To]
 			}
 			if (*dfn)[p] < (*low)[g.Edge[i].To] {
 				result = append(result, g.Edge[i])
+			}
+		} else {
+			if (*low)[p] > (*dfn)[g.Edge[i].To] {
+				(*low)[p] = (*dfn)[g.Edge[i].To]
+			}
+		}
+	}
+	return result
+}
+
+func UTarjanP(g *graph.CFSGraph, p, f, r int, num *int, dfn, low *[]int) []int {
+	result := make([]int, 0)
+	*num++
+	(*dfn)[p], (*low)[p] = *num, *num
+	count := 0
+	for i := g.Head[p]; i > 0; i = g.Edge[i].Next {
+		if g.Edge[i].To == f {
+			continue
+		}
+		if (*dfn)[g.Edge[i].To] == 0 {
+			result = append(result, UTarjanP(g, g.Edge[i].To, p, r, num, dfn, low)...)
+			if (*low)[p] > (*low)[g.Edge[i].To] {
+				(*low)[p] = (*low)[g.Edge[i].To]
+			}
+			if (*dfn)[p] <= (*low)[g.Edge[i].To] {
+				count++
+				if p != r || count > 0 {
+					result = append(result, p)
+				}
 			}
 		} else {
 			if (*low)[p] > (*dfn)[g.Edge[i].To] {
--- algorithms/graph/tarjan_test.go
+++ algorithms/graph/tarjan_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/imkuqin-zw/Data-Structures-and-Algorithms/data_structure/graph"
 )
 
-func TestTarjan(t *testing.T) {
+func TestUTarjan(t *testing.T) {
 	g := graph.NewCFSGraph(7, false)
 	g.AddEdge(1, 4, 0)
 	g.AddEdge(1, 2, 0)
@@ -21,10 +21,31 @@ func TestTarjan(t *testing.T) {
 	num := 0
 	for i := 1; i <= 7; i++ {
 		if dfn[i] == 0 {
-			for _, item := range Tarjan(g, i, 0, &num, &dfn, &low) {
+			for _, item := range UTarjan(g, i, 0, &num, &dfn, &low) {
 				fmt.Println(item)
 			}
 		}
 
+	}
+}
+
+func TestUTarjanP(t *testing.T) {
+	g := graph.NewCFSGraph(7, false)
+	g.AddEdge(1, 4, 0)
+	g.AddEdge(1, 2, 0)
+	g.AddEdge(2, 3, 0)
+	g.AddEdge(3, 5, 0)
+	g.AddEdge(5, 7, 0)
+	g.AddEdge(5, 6, 0)
+	g.AddEdge(6, 4, 0)
+
+	dfn, low := make([]int, 8), make([]int, 8)
+	num := 0
+	for i := 1; i <= 7; i++ {
+		if dfn[i] == 0 {
+			for _, item := range UTarjanP(g, i, 0, i, &num, &dfn, &low) {
+				fmt.Println(item)
+			}
+		}
 	}
 }
